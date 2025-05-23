d6ca460b5c3d7f24df9e44709a27aea4394a39c3
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-05-23 18:08:04 -0300

    feat: exibe data digitada e data atual

---
+++ data_hora/datedate.py
@@ -0,0 +1,12 @@
+from datetime import *
+
+print('=== DATA ===\n')
+data_digitada = date(2001, 5, 3)    # Exibe dia, mês e ano informado
+print('Data digitada:', data_digitada)
+data_atual = date.today()     # Exibe dia, mês e ano atual
+print('Data atual:', data_atual)
+
+print('\n=== DATA E HORAS ===\n')
+data_hora = datetime(1985, 7, 11, 17, 30, 47)
+print('Data e hora digitada:', data_hora)
+print('Data e hora atual: ', datetime.today())
